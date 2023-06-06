176f7e04db4425b4bd8365657411e476a6c52b0a
junaid786-prog <junaidshoaib555@gmail.com>
2023-06-06 23:06:56 +0500

    new validation functions added

--- Models/Book.cs
+++ Models/Book.cs
@@ -17,12 +17,33 @@ namespace LMS.Models
 
         public Book(int id, string title, string author, int publicationYear, string genre, int status = 0)
         {
+            try {
+                ValidateBook(title, author, publicationYear, genre, status);
+            } catch (Exception e){
+                throw new Exception("Exception: " + e.Message);
+            }
             this.id = id;
             this.title = title;
             this.author = author;
             this.publicationYear = publicationYear;
             this.genre = genre;
             this.status = status;
+        }
+
+        private bool ValidateBook(string title, string author, int publicationYear, string genre, int status){
+            // if (!IsTitleValid(title)) throw new Exception("Valid title is between 0-100 chars");
+            // if (!IsPublicationYearValid(publicationYear)) throw new Exception("Publication year can not be in future or some negative number");
+            return true;
+        }
+
+        private bool IsTitleValid(string title){
+            if (title.Length < 0 || title.Length > 100) return false;
+            return true;
+        }
+
+        private bool IsPublicationYearValid(int year){
+            if (year < 0 || new DateOnly().Year > year) return false;
+            return true;
         }
     }
 }

0ae2b2b729200e8c5be02cae7d4d43c9ac357639
junaid786-prog <junaidshoaib555@gmail.com>
2023-06-06 22:42:13 +0500

    utils folder comments removed

--- utils/Utils.cs
+++ utils/Utils.cs
@@ -5,8 +5,6 @@ namespace LMS.Utils
         private static int counter = 0;
         public static int GenerateUniqueId()
         {
-            // int uniqueId = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
-            // uniqueId = uniqueId * 1000 + counter;
             int currentSeconds = (int) DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             int uniqueId = currentSeconds + counter;
             counter++;
